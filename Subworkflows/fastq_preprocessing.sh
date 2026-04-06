4303e6167c5e3ac4ece359ade42d1de587604098
naira25 <nraan@uoc.edu>
2026-04-06 10:58:19 +0000

    Actualització

--- Subworkflows/fastq_preprocessing.sh
+++ Subworkflows/fastq_preprocessing.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-/*
- * Fastq Pre Processing Subworkflow
-*/
+BASE_DIR="/workspaces/Benchmarking-Somatic-VariantCallers-ctDNA"
+OUTPUT_DIR="${BASE_DIR}/Results/Test_Results"
 
-nextflow.enable.dsl=2
-
-workflow {
-    nextflow run nf-core/sarek -r 3.8.1 <VERSION> \ 
-        --profile <docker/singulatiry/.../institute> \
-        --input samplesheet.csv
-        --step "mapping" # Starting step of the workflow, can be "mapping"
-        --outdir
-}
-
+nextflow run nf-core/sarek -r 3.8.1 \
+    -c ${BASE_DIR}/Conf/nextflow.config \
+    -profile docker \
+    --input ${BASE_DIR}/Metadata/samplesheet.csv \
+    --outdir ${OUTPUT_DIR} \
+    --genome GATK.GRCh38 \
+    --wes true \
+    --intervals ${BASE_DIR}/Metadata/ROC2.bed \
+    --save_output_as_bam true
+    -resume
 
 # Version is laterst version
 # Started from FastQ files gzip.compressed, paired-end sequencing data of ctDNA samples B, D and E
