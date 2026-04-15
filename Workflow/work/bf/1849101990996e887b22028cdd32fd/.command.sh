2cd037e73b85fce5fa816294b1f54a544ab65d74
naira25 <nraan@uoc.edu>
2026-04-15 17:36:42 +0200

    Variant Callers

--- Workflow/work/bf/1849101990996e887b22028cdd32fd/.command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ue
-lofreq somatic \
-    -n B_SRX9642665_SRR13209649_duplicates.bam \
-    -t E_SRX9642659_SRR13209655_duplicates.bam \
-    -f chr7.fa \
-    -l chr7_target.bed \
-    --threads 4 \
-    -o B_SRX9642665_SRR13209649_duplicates_vs_E_SRX9642659_SRR13209655_duplicates \
-    --call-indels
